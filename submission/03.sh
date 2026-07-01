93ff83cac77f3c8cad7aa61b65df3637b03f24e7
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-01 13:29:18 +0300

    refactor sol 3

--- submission/03.sh
+++ submission/03.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# Create a SegWit address and add funds to it
-
-# Generate coins to the default wallet (mines 101 blocks for coinbase maturity)
-MINING_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
-bitcoin-cli -regtest generatetoaddress 101 "$MINING_ADDRESS" > /dev/null
-
 # Create a SegWit address
-SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
 
-# Send funds to the SegWit address (send 10 BTC)
-bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$SEGWIT_ADDRESS" 10
+# Mine 101 blocks to that address
+bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS" >/dev/null
 
-# Return the SegWit address
-echo "$SEGWIT_ADDRESS"
+# Return only the address
+echo "$ADDRESS"
