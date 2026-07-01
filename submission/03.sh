9383e0dc5c36103f69e9f4905d32f227a144974b
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-01 12:46:52 +0300

    send to address on file 3

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,8 @@
 # Create a SegWit address.
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
 # Get a block reward address
+# send to this address
+bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress $ADDRESS 50
 BLOCK_REWARD_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
 # Add funds to the address.
 bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 101 $ADDRESS
