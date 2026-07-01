cf48e25924442b0e467f2442c2dbf24d98debee6
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-01 13:22:53 +0300

    refactor sol 3

--- submission/03.sh
+++ submission/03.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
+# Create a SegWit address and add funds to it
+
+# Generate coins to the default wallet (mines 101 blocks for coinbase maturity)
+MINING_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$MINING_ADDRESS" > /dev/null
+
 # Create a SegWit address
-ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
 
-# Mine first
-bitcoin-cli -regtest -rpcwallet=btrustwallet settxfee 0.00001000
-bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 101 > /dev/null 2>&1
+# Send funds to the SegWit address (send 10 BTC)
+bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$SEGWIT_ADDRESS" 10
 
-# Now the wallet has mature coins
-bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$ADDRESS" 1
-# Confirm the transaction
-CONFIRM_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
-bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 1 "$CONFIRM_ADDR" >/dev/null
-
-echo "$ADDRESS"
+# Return the SegWit address
+echo "$SEGWIT_ADDRESS"
